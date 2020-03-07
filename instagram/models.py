753ba909e9df6ec13b0f58baab23626a006b8d87
ian mwangi <iankimotho233@gmail.com>
2020-03-07 12:31:19 +0300

    Create the Profile model

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Profile(models.Model):
+    profile_pic = CloudinaryField('image', null=True)
+    bio = models.TextField()
+    user = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.bio
