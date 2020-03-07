fa87f212105dea9270683cf8d98d19f2031db074
ian mwangi <iankimotho233@gmail.com>
2020-03-07 14:08:08 +0300

    Include links to bootstrap and javascript in base.html file

--- imk/settings.py
+++ imk/settings.py
@@ -40,6 +40,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'instagram',
     'cloudinary',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
