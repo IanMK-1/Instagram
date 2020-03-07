a696087858d6fb6e75ad044f3aa83780be2a922b
ian mwangi <iankimotho233@gmail.com>
2020-03-07 14:31:54 +0300

    Register Profile and Image models to admin page

--- imk/settings.py
+++ imk/settings.py
@@ -16,7 +16,6 @@ import cloudinary
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -33,7 +32,6 @@ cloudinary.config(
 )
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -79,7 +77,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'imk.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -91,7 +88,6 @@ DATABASES = {
         'PASSWORD': 'ian',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -111,7 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -125,8 +120,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
--- instagram/admin.py
+++ instagram/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Image, Profile
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    filter_horizontal = ('user',)
+
 
 # Register your models here.
+admin.site.register(Image)
+admin.site.register(Profile, ProfileAdmin)
