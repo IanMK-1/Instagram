5130425abe23a1cf048ced9503b441d698b64509
ian mwangi <iankimotho233@gmail.com>
2020-03-07 15:49:48 +0300

    Create the url path for login

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.home, name='Home'),
+    path('', auth_views.login, name='login'),
 ]
