bf42848181494d8c66bf81e6b85158fe4c401e2e
ian mwangi <iankimotho233@gmail.com>
2020-03-08 14:09:13 +0300

    Add styling to profile page

--- instagram/views.py
+++ instagram/views.py
@@ -18,24 +18,30 @@ def profile(request):
         if current_user.is_authenticated:
             user_profile = Profile.objects.get(id=current_user.id)
             profile_images = Image.objects.filter(profile__id=current_user.id).all()
+            image_count = profile_images.count()
             all_following = user_profile.user.all()
+            following_count = all_following.count()
             users = User.objects.all()
-            number = 0
+            followers_count = 0
             for user in users:
                 for specific_user_profile in user.profile_set.all():
                     for following_user in specific_user_profile.user.all():
                         if following_user.id == current_user.id:
-                            number += 1
+                            followers_count += 1
                         else:
-                            number = 0
+                            followers_count = 0
 
     except ObjectDoesNotExist:
         profile_images = None
         user_profile = None
+        all_following = None
+        followers_count = 0
+        image_count = 0
+        following_count = 0
 
     return render(request, 'profile.html',
                   {"user_profile": user_profile, "profile_images": profile_images, "current_user": current_user,
-                   "all_following": all_following, "number": number})
+                   "all_following": all_following, "followers_count": followers_count, "image_count": image_count, "following_count": following_count})
 
 
 def edit_profile(request):
