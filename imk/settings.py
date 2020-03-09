e4cba89c7af86bbc03fc50240e18d177b9c796b1
ian mwangi <iankimotho233@gmail.com>
2020-03-09 17:26:50 +0300

    Configure settings for deployment

--- imk/settings.py
+++ imk/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 MODE = config("MODE", default="dev")
 
 cloudinary.config(
@@ -88,26 +88,8 @@ WSGI_APPLICATION = 'imk.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# if config('MODE') == "dev":
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': config('DB_NAME'),
-#             'USER': config('DB_USER'),
-#             'PASSWORD': config('DB_PASSWORD'),
-#             'HOST': config('DB_HOST'),
-#             'PORT': '',
-#         }
-#
-#     }
-# # production
-# else:
-#     DATABASES = {
-#         'default': dj_database_url.config(
-#             default=config('DATABASE_URL')
-#         )
-#     }
-DATABASES = {
+if config('MODE') == "dev":
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': config('DB_NAME'),
@@ -117,6 +99,13 @@ DATABASES = {
             'PORT': '',
         }
 
+    }
+# production
+else:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=config('DATABASE_URL')
+        )
     }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
